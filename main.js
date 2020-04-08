d04fdec0d8bcbbbb4061691ac4fcffdc71ec55fb
leonadia <leonadia1121@gmail.com>
2020-04-08 17:55:47 +0800

    4/8

--- main.js
+++ main.js
@@ -74,7 +74,7 @@ const createWindow = () => {
 
     mainWindow.on('close', event => {
         console.log(xml);
-        if (xml === 'saved') {
+        if (xml === 'saved' || xml === null) {
             app.quit();
         } else {
             mainWindow.webContents.send('close_event', 'end');
